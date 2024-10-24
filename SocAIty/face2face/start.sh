cf6d724833c6d50184f78daaf08eba5a8dccdd5d
lvchengxin_dxm <lvchengxin@duxiaoman>
2024-10-24 14:21:21 +0800

    fix bug

--- SocAIty/face2face/start.sh
+++ SocAIty/face2face/start.sh
@@ -1,6 +1,6 @@
 # 获取进程ID
 # 使用绝对路径加载 conda 环境
-pid1=$(ps -ef | grep 'python3.10 -u /nvme0n1-disk/SocAlty/face2face/API.py' | grep -v grep | awk '{print $2}')
+pid1=$(ps -ef | grep 'python3.10 -u /nvme0n1-disk/ai_tools/SocAIty/face2face/API.py' | grep -v grep | awk '{print $2}')
 
 # 检查进程ID是否存在并杀掉进程
 if [ -n "$pid1" ]; then
@@ -10,6 +10,5 @@ else
   echo "No matching process found for 'API.py'."
 fi
 
-nohup python3.10 -u /nvme0n1-disk/SocAlty/face2face/API.py > face_swap_faces.log 2>&1 &
-echo "Webapp started." 
-conda deactivate
+nohup python3.10 -u /nvme0n1-disk/ai_tools/SocAIty/face2face/API.py > face_swap_faces.log 2>&1 &
+echo "Webapp started."
